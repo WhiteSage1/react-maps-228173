0ca937e602418ed521b3fa7911f142db58c0d759
WEB Contest <contest@kitm.local>
2025-03-26 18:34:52 +0200

    my first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, {useState} from "react";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import './App.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React for competition WEB dev challenge 2025</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <MapContainer center={[54.8944, 23.8848]} zoom={8}scrollWheelZoom={true}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+      />
+      {fetch("https://konkursas.kitm.lt/backend/380288/api/v1/places")
+        .then(response => response.json())
+        .then(data => data.data.map((value) => (
+          <Marker 
+            key={value._id}
+            position={[value.latitude, value.longitude]}>
+            onclick
+            <Popup>
+              <div className="popup-div">
+                <h2>{value.name}</h2>
+                <h4>{value.description}</h4>
+                <p>Adresas: {value.address}</p>
+                <p>Reitingas: {value.rating}</p>
+              </div>
+            </Popup>
+          </Marker>
+        )))
+        .catch(error => console.log(error))}
+      
+    </MapContainer>
+  );
 }
 
-export default App
+export default App;
